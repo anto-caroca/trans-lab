d0395d8aef8167249c44d3e058e59b94db45e8af
antonella <antonella_caroca@hotmail.com>
2018-06-28 07:00:55 -0400

    agrego main.js aun en construccion

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
-/* Holiii acá va tu código también */
+let password= document.getElementById('password');
+
+
+ function validatePassword(){
+
+			let exp = /^[0-9]{4,8}$/;
+			let result = exp.test(password.value);
+      
+      if(!result){
+        alert( 'la contraseña solo admite números (min 4 máx 8)');
+        return false;
+        
+			}else{
+        return true;
+      }
+    }
+    
+      
+window.onload = () => {  
+      
+  const url =  'http://www.psep.cl/api/Bip.php?&numberBip=14760348';
+  const renderSaldo = document.getElementById("renderSaldo");
+  let storeBipBalance;
+
+async function fetchApi(){
+  const bip = await fetch(`${url}`)
+  const dataBip = await bip.json();
+  
+  let arrBip = Object.entries(dataBip)
+  console.log(arrBip[2][1])
+  storeBipBalance = arrBip[2][1];
+  renderSaldo.innerHTML = "su saldo es: "+storeBipBalance;
+
+  }
+  fetchApi();
+
+}
+
