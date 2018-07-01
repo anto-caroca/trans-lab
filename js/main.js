741c64579de6d582f5b34cbd39485d8011afcba0
antonella <antonella_caroca@hotmail.com>
2018-07-01 18:07:17 -0400

    agrego funciones para el menu desplegable

--- js/main.js
+++ js/main.js
@@ -14,7 +14,14 @@ let password= document.getElementById('password');
         return true;
       }
     }
-    
+    function openSlideMenu(){
+      document.getElementById('side-menu').style.width='250px';
+      document.getElementById('main').style.marginLeft='250px';
+    }
+    function closeSlideMenu(){
+      document.getElementById('side-menu').style.width='0';
+      document.getElementById('main').style.marginLeft='0';
+    }
 
 //window.onload = () => {  
   
